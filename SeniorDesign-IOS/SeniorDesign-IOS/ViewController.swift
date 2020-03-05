c7ba1de807906616912f0516a8504ee1acc5c5d5
Riley Wagner <riley.wagner@siu.edu>
2020-03-05 12:36:14 -0600

    initial commit

---
+++ SeniorDesign-IOS/SeniorDesign-IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeniorDesign-IOS
+//
+//  Created by Senior Design  on 3/5/20.
+//  Copyright © 2020 Riley Wagner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
